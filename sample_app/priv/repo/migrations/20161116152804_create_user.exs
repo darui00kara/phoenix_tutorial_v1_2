cf0bd76fe2ff176068e370fd7ee52b505785d95d
Takeshi Miyamoto <Takeshi00Miyamoto@gmail.com>
2016-11-17 01:05:32 +0900

    Create users table

---
+++ sample_app/priv/repo/migrations/20161116152804_create_user.exs
@@ -0,0 +1,16 @@
+defmodule SampleApp.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+  @disable_ddl_transaction true
+ 
+  def change do
+    create table(:users) do
+      add :name, :string
+      add :email, :string
+
+      timestamps()
+    end
+    
+    create unique_index :users, [:name], concurrently: true
+    create unique_index :users, [:email], concurrently: true
+  end
+end
