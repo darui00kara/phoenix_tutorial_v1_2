4df57fa433140679350d97f872174068f7ba171f
Takeshi Miyamoto <Takeshi00Miyamoto@gmail.com>
2016-11-18 00:56:41 +0900

    Add user varify.

--- sample_app/web/models/user.ex
+++ sample_app/web/models/user.ex
@@ -15,5 +15,10 @@ defmodule SampleApp.User do
     struct
     |> cast(params, [:name, :email])
     |> validate_required([:name, :email])
+    |> validate_format(:email, ~r/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i)
+    |> unique_constraint(:name)
+    |> unique_constraint(:email)
+    |> validate_length(:name, min: 1)
+    |> validate_length(:email, max: 50)
   end
 end
