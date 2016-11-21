b60f16eb1b4d610a8ebae350a9282b3bf78c8315
Takeshi Miyamoto <Takeshi00Miyamoto@gmail.com>
2016-11-22 00:02:29 +0900

    Add gravatar image.

--- sample_app/web/views/user_view.ex
+++ sample_app/web/views/user_view.ex
@@ -1,3 +1,10 @@
 defmodule SampleApp.UserView do
   use SampleApp.Web, :view
+
+  alias SampleApp.User
+  alias SampleApp.Helpers.Gravatar
+
+  def gravatar_for(%User{email: email}) do
+    Gravatar.get_gravatar_url(email, 50)
+  end
 end
