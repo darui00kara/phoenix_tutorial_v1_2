1348531f6d9107fc753dbd5435da554da3e673a1
Takeshi Miyamoto <Takeshi00Miyamoto@gmail.com>
2016-11-24 23:11:22 +0900

    Finish sign_up.

--- sample_app/web/controllers/user_controller.ex
+++ sample_app/web/controllers/user_controller.ex
@@ -3,13 +3,26 @@ defmodule SampleApp.UserController do
 
   alias SampleApp.User
 
+  def show(conn, %{"id" => id}) do
+    user = Repo.get!(User, id)
+    render(conn, "show.html", user: user)
+  end
+
   def new(conn, _params) do
     changeset = User.changeset(%User{})
     render(conn, "new.html", changeset: changeset)
   end
 
-  def show(conn, %{"id" => id}) do
-    user = Repo.get!(User, id)
-    render(conn, "show.html", user: user)
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+
+    case Repo.insert(changeset) do
+      {:ok, _user} ->
+        conn
+        |> put_flash(:info, "User created successfully.")
+        |> redirect(to: static_page_path(conn, :home))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
