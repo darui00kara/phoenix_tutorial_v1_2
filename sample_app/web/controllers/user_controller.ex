8a61f437dfb65710ebe54f00018296c201623648
Takeshi Miyamoto <Takeshi00Miyamoto@gmail.com>
2016-11-29 00:51:46 +0900

    Add edit and update function.

--- sample_app/web/controllers/user_controller.ex
+++ sample_app/web/controllers/user_controller.ex
@@ -28,6 +28,22 @@ defmodule SampleApp.UserController do
   end
 
   def edit(conn, %{"id" => id}) do
-    redirect(conn, to: static_page_path(conn, :home))
+    user = Repo.get(User, id)
+    changeset = User.changeset(user)
+    render conn, "edit.html", user: user, changeset: changeset
+  end
+
+  def update(conn, %{"id" => id, "user" => user_params}) do
+    user = Repo.get(User, id)
+    changeset = User.changeset(user, user_params)
+
+    case Repo.update(changeset) do
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "User updated successfully.")
+        |> redirect(to: user_path(conn, :show, user.id))
+      {:error, failed_changeset} ->
+        render(conn, "edit.html", user: user, changeset: failed_changeset)
+    end
   end
 end
